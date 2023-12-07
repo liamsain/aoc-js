42be5310569f67a20f61227ec955fa1998ea6b9d
Liam Kennedy <liamkennedy89@outlook.com>
2023-12-07 20:08:55 +0000

    day7

--- 2023/7.js
+++ 2023/7.js
@@ -2,7 +2,8 @@ import { getAdventOfCodeData } from '../utils.js';
 const input = await getAdventOfCodeData(2023, 7);
 const start = performance.now();
 
-const cardValueLookup = ['', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+let firstResult = 0;
+const cardValueLookup = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
 const highcards = [];
 const onepairs = [];
 const twopairs = [];
@@ -44,36 +45,41 @@ input.split('\n').forEach(line => {
 });
 
 function sortFn(a, b) {
-// if a less than b, return -1, else if b is more than a, return 1. else return 0
   const handA = a.split(' ')[0].split('');
   const handB = b.split(' ')[0].split('');
   let result = 0;
   for (let i =0; i < handA.length;i++) {
-    const aInd = cardValueLookup.findIndex(handA[i]);
-    const bInd = cardValueLookup.findIndex(handB[i]);
+    const aInd = cardValueLookup.findIndex(x => x == handA[i]);
+    const bInd = cardValueLookup.findIndex(x => x == handB[i]);
     if (aInd !== bInd) {
       if (bInd > aInd) {
-        result = 1;
-      } else {
         result = -1;
+      } else {
+        result = 1;
       }
       break;
     }
   }
   return result;
 }
+highcards.sort(sortFn);
+onepairs.sort(sortFn);
+twopairs.sort(sortFn);
+threes.sort(sortFn);
+fullhouse.sort(sortFn);
+fours.sort(sortFn);
+fives.sort(sortFn);
+const allHands = highcards.concat(onepairs).concat(twopairs).concat(threes).concat(fullhouse).concat(fours).concat(fives);
+allHands.forEach((hand, index) => {
+  const handValue = hand.split(' ')[1];
+  firstResult += parseInt(handValue) * (index + 1);
+});
+
 
 
 const end = performance.now();
-/*
-console.log('fives =====', fives);
-console.log('fours =====', fours);
-console.log('fullhouse =====', fullhouse);
-console.log('threes =====', threes);
-console.log('twopairs =====', twopairs);
-console.log('onepairs =====', onepairs);
-console.log('highcards =====', highcards);
-*/
+
+console.log('first', firstResult);
 
 console.log('time taken', end - start, 'ms');
     
