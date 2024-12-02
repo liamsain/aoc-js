17564b36df9c9f0e8cf367628b256ce5aa7c55a8
liamsain <liamkennedy89@outlook.com>
2024-12-02 21:25:06 +0000

    2024 day 2 refactor

--- 2024/2.js
+++ 2024/2.js
@@ -4,70 +4,35 @@ const start = performance.now();
 
 let safeReports = 0;
 let safeReportsWithProblemDampner = 0;
+
+input.split('\n').forEach(l => {
+  const nums = l.split(' ').map(x => Number(x));
+  if (reportIsSafe(nums)) {
+    safeReports += 1;
+  } else {
+    for (let i = 0; i < nums.length; i++) {
+      const clonedNums = [...nums];
+      clonedNums.splice(i, 1);
+      if (reportIsSafe(clonedNums)) {
+        safeReportsWithProblemDampner += 1;
+        break;
+      }
+    }
+  }
+});
+
 function reportIsSafe(nums) {
   let firstPairIncreases = nums[1] > nums[0];
-  let firstPairDiff = Math.abs(nums[0] - nums[1]);
-
-  let safe = firstPairDiff >= 1 && firstPairDiff <= 3;
-  if (safe) {
-    for (let i = 2; i < nums.length; i++) {
-      const currentPairIncreases = nums[i] > nums[i - 1];
-      const currentPairDiff = Math.abs(nums[i] - nums[i - 1]);
-      if (firstPairIncreases !== currentPairIncreases) {
-        safe = false;
-      }
-      if (currentPairDiff < 1 || currentPairDiff > 3) {
-        safe = false;
-      }
+  let safe = true;
+  for (let i = 0; i < nums.length - 1; i++) {
+    const currentPairIncreases = nums[i + 1] > nums[i];
+    const currentPairDiff = Math.abs(nums[i + 1] - nums[i]);
+    if (firstPairIncreases !== currentPairIncreases || currentPairDiff < 1 || currentPairDiff > 3) {
+      safe = false;
     }
   }
   return safe;
 }
-
-input.split('\n').forEach(l => {
-  const nums = l.split(' ').map(x => Number(x));
-  let firstPairIncreases = nums[1] > nums[0];
-
-  let safe = true;
-  let repaired = false;
-  for (let i = 0; i < nums.length - 1; i++) {
-    const currentPairIncreases = nums[i + 1] > nums[i];
-    const currentPairDiff = Math.abs(nums[i + 1] - nums[i]);
-
-    if (firstPairIncreases !== currentPairIncreases || currentPairDiff < 1 || currentPairDiff > 3) {
-      if (safe) {
-        let clonedNums = [...nums];
-        clonedNums.splice(i, 1);
-        if (reportIsSafe(clonedNums)) {
-          repaired = true;
-        } else {
-          clonedNums = [...nums];
-          clonedNums.splice(i + 1, 1);
-          if (reportIsSafe(clonedNums)) {
-            repaired = true;
-          }
-          if (!repaired && i === 1) {
-            clonedNums = [...nums];
-            clonedNums.splice(0, 1);
-            if (reportIsSafe(clonedNums)) {
-              repaired = true;
-            }
-
-          }
-        }
-      }
-      safe = false;
-    }
-
-  }
-  if (safe) {
-    safeReports += 1;
-  }
-  if (repaired) {
-    safeReportsWithProblemDampner += 1;
-  }
-});
-
 
 
 const end = performance.now();
