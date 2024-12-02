2399547cf4fa89bf6ac36fea59e523f65193918a
liamsain <liamkennedy89@outlook.com>
2024-12-02 21:08:49 +0000

    2024 day 2

--- 2024/2.js
+++ 2024/2.js
@@ -1,11 +1,5 @@
 import { getAdventOfCodeData } from '../node-utils.js';
 const input = await getAdventOfCodeData(2024, 2);
-// const input = `7 6 4 2 1
-// 1 2 7 8 9
-// 9 7 6 2 1
-// 1 3 2 4 5
-// 8 6 4 4 1
-// 1 3 6 7 9`;
 const start = performance.now();
 
 let safeReports = 0;
@@ -29,25 +23,48 @@ function reportIsSafe(nums) {
   }
   return safe;
 }
-const pair = {first: 1, next: 3, diff: 2, increases: true }
+
 input.split('\n').forEach(l => {
   const nums = l.split(' ').map(x => Number(x));
   let firstPairIncreases = nums[1] > nums[0];
 
   let safe = true;
-  for (let i = 1; i < nums.length; i++) {
-    const currentPairIncreases = nums[i] > nums[i - 1];
-    const currentPairDiff = Math.abs(nums[i] - nums[i - 1]);
-    if (firstPairIncreases !== currentPairIncreases) {
-      safe = false;
-    }
-    if (currentPairDiff < 1 || currentPairDiff > 3) {
+  let repaired = false;
+  for (let i = 0; i < nums.length - 1; i++) {
+    const currentPairIncreases = nums[i + 1] > nums[i];
+    const currentPairDiff = Math.abs(nums[i + 1] - nums[i]);
+
+    if (firstPairIncreases !== currentPairIncreases || currentPairDiff < 1 || currentPairDiff > 3) {
+      if (safe) {
+        let clonedNums = [...nums];
+        clonedNums.splice(i, 1);
+        if (reportIsSafe(clonedNums)) {
+          repaired = true;
+        } else {
+          clonedNums = [...nums];
+          clonedNums.splice(i + 1, 1);
+          if (reportIsSafe(clonedNums)) {
+            repaired = true;
+          }
+          if (!repaired && i === 1) {
+            clonedNums = [...nums];
+            clonedNums.splice(0, 1);
+            if (reportIsSafe(clonedNums)) {
+              repaired = true;
+            }
+
+          }
+        }
+      }
       safe = false;
     }
 
   }
   if (safe) {
     safeReports += 1;
+  }
+  if (repaired) {
+    safeReportsWithProblemDampner += 1;
   }
 });
 
