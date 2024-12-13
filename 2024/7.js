b6eca86b1347827b4e0ea3bd8a69a097cec41b56
Liam Kennedy <liamkennedy89@outlook.com>
2024-12-13 20:16:20 +0000

    refactored 2024 7

--- 2024/7.js
+++ 2024/7.js
@@ -1,16 +1,5 @@
 import { getAdventOfCodeData } from '../node-utils.js';
 const input = await getAdventOfCodeData(2024, 7);
-import { Worker } from 'node:worker_threads';
-import os from 'os';
-// const input = `190: 10 19
-// 3267: 81 40 27
-// 83: 17 5
-// 156: 15 6
-// 7290: 6 8 6 15
-// 161011: 16 10 13
-// 192: 17 8 14
-// 21037: 9 7 18 13
-// 292: 11 6 16 20`;
 
 const start = performance.now();
 let part1 = 0;
@@ -41,16 +30,10 @@ export function getOperatorCombos(num) {
   let padding = max.toString(2).length;
   while (curr < max) {
     let str = curr.toString(2).padStart(padding, '0');
-    let combo = new Array(str.length).fill('');
-    let combo2 = new Array(str.length).fill('');
-    for (let i = 0; i < str.length; i++) {
-      combo[i] = str[i] == '0' ? '*' : '+'
-      combo2[i] = str[i] == '1' ? '*' : '+'
-    }
-    combos.push(combo.join(''));
-    combos.push(combo2.join(''));
-    curr += 1;
+    curr++;
+    combos.push(str);
   }
+
   opComboMap[num] = combos;
   return combos;
 }
@@ -59,15 +42,18 @@ export function atLeastOneEquationMakesTestVal(nums, testVal) {
   let equationsAreTrue = false;
   for (let i = 0; i < operatorCombos.length; i++) {
     let res = nums[0];
+    let res2 = nums[0];
     for (let numi = 1; numi < nums.length; numi++) {
-      if (operatorCombos[i][numi - 1] == '+') {
+      if (operatorCombos[i][numi - 1] == '0') {
         res += nums[numi]
+        res2 *= nums[numi]
       } else {
         res *= nums[numi]
+        res2 += nums[numi]
       }
     }
 
-    if (res == testVal) {
+    if (res == testVal || res2 == testVal) {
       equationsAreTrue = true;
       break;
     }
