1eb5f4545f916413f8b33b7477264c86518c0e90
liamsain <liamkennedy89@outlook.com>
2024-12-14 14:30:33 +0000

    2024 day 7 - got solution, started increasing performance

--- 2024/7-worker.js
+++ 2024/7-worker.js
@@ -1,61 +1,52 @@
+import { simGuardStepsV2 } from './helpers.js';
 import { parentPort } from 'node:worker_threads';
+
 parentPort.on('message', (data) => {
 
   let res = 0;
-  for (let i = 0; i < data.lines.length; i++) {
-    const splitLn = data.lines[i].split(': ');
-    const testVal = Number(splitLn[0]);
-
-    const nums = splitLn[1].split(' ').map(x => Number(x));
-    if (atLeastOneEquationMakesTestVal(nums, testVal)) {
-      res += testVal;
+  for (let i = 0; i < data.candidates.length;i++) {
+    const curr = data.candidates[i];
+    if (part2Fn(curr.nums, curr.testVal, data.comboMap)) {
+      res += curr.testVal
     }
   }
-
   parentPort.postMessage(res);
 });
-const opComboMap = {};
 
-export function getOperatorCombos(num) {
-  if (opComboMap[num]) {
-    return opComboMap[num];
-  }
-  let combos = [];
-  let curr = 0;
-  let max = 2 ** (num - 1);
-  let padding = max.toString(2).length;
-  while (curr < max) {
-    let str = curr.toString(2).padStart(padding, '0');
-    let combo = new Array(str.length).fill('');
-    let combo2 = new Array(str.length).fill('');
-    for (let i = 0; i < str.length; i++) {
-      combo[i] = str[i] == '0' ? '*' : '+'
-      combo2[i] = str[i] == '1' ? '*' : '+'
-    }
-    combos.push(combo.join(''));
-    combos.push(combo2.join(''));
-    curr += 1;
-  }
-  opComboMap[num] = combos;
-  return combos;
-}
-export function atLeastOneEquationMakesTestVal(nums, testVal) {
-  const operatorCombos = getOperatorCombos(nums.length - 1);
-  let equationsAreTrue = false;
+
+
+function part2Fn(nums, testVal, comboMap) {
+  const operatorCombos = comboMap[`${nums.length - 1} 3`].filter(x => x.includes('2'));
+  let result = false;
   for (let i = 0; i < operatorCombos.length; i++) {
-    let res = nums[0];
+    let res = nums[0]
+    let res2 = nums[0]
+    let res3 = nums[0]
     for (let numi = 1; numi < nums.length; numi++) {
-      if (operatorCombos[i][numi - 1] == '+') {
+      const op = operatorCombos[i][numi - 1];
+      const currStr = nums[numi].toString();
+      if (op == '0') {
         res += nums[numi]
-      } else {
+        res2 *= nums[numi]
+        const res3Str = res3.toString();
+        res3 = Number(res3Str + currStr);
+      } else if (op == '1') {
         res *= nums[numi]
+        const res2Str = res2.toString();
+        res2 = Number(res2Str + currStr);
+        res3 += nums[numi]
+
+      } else {
+        const resStr = res.toString();
+        res = Number(resStr + currStr);
+        res2 += nums[numi];
+        res3 *= nums[numi];
       }
     }
-
-    if (res == testVal) {
-      equationsAreTrue = true;
+    if (res == testVal || res2 == testVal || res3 == testVal) {
+      result = true;
       break;
     }
   }
-  return equationsAreTrue;
+  return result;
 }
--- 2024/7.js
+++ 2024/7.js
@@ -1,10 +1,19 @@
 import { getAdventOfCodeData } from '../node-utils.js';
+import { Worker } from 'node:worker_threads';
+import assert from 'node:assert'
+import os from 'os';
+
 const input = await getAdventOfCodeData(2024, 7);
-
 const start = performance.now();
-let part1 = 0;
 
+let part1 = 0;
+let part2 = 0;
 const opComboMap = {};
+const part2Candidates = [];
+for (let i = 1; i < 13;i++) {
+  getOperatorCombosFromColumnNumber(i, 2);
+  getOperatorCombosFromColumnNumber(i, 3);
+}
 
 input.split('\n').forEach((line, lineIndex) => {
   const splitLn = line.split(': ');
@@ -13,32 +22,57 @@ input.split('\n').forEach((line, lineIndex) => {
   const nums = splitLn[1].split(' ').map(x => Number(x));
   if (atLeastOneEquationMakesTestVal(nums, testVal)) {
     part1 += testVal;
+  } else {
+    part2Candidates.push({ nums, testVal });
   }
 });
-const end = performance.now();
-console.log('part 1: ', part1);
-console.log('time taken', end - start, 'ms');
+const numWorkers = os.cpus().length - 1;
+const chunkSize = Math.ceil(part2Candidates.length / numWorkers);
+let completedWorkers = 0;
+for (let i = 0; i < numWorkers; i++) {
+  const chunk = part2Candidates.slice(i * chunkSize, (i + 1) * chunkSize);
+  const worker = new Worker('./2024/7-worker.js');
+  worker.postMessage({ candidates: chunk, comboMap: opComboMap });
+  worker.on('message', res => {
 
+    debugger;
+    completedWorkers++;
+    part2 += res;
+    worker.terminate();
+    if (completedWorkers == numWorkers) {
+      const end = performance.now();
+      assert(part1 == 267566105056);
+      assert(part2 + part1 == 116094961956019)
+      console.log('part 1: ', part1);
+      console.log('part 2: ', part2 + part1);
+      console.log('time taken', end - start, 'ms');
+    }
+  });
+}
 
-export function getOperatorCombos(num) {
-  if (opComboMap[num]) {
-    return opComboMap[num];
+function getOperatorCombosFromColumnNumber(cols, base) {
+  const mapKey = `${cols} ${base}`;
+  if (opComboMap[mapKey]) {
+    return opComboMap[mapKey];
   }
-  let combos = [];
+  const maxBaseStr = new Array(cols).fill(base - 1).join('');
+  const max = parseInt(maxBaseStr, base);
+  const combos = [];
   let curr = 0;
-  let max = 2 ** (num - 1);
-  let padding = max.toString(2).length;
-  while (curr < max) {
-    let str = curr.toString(2).padStart(padding, '0');
-    curr++;
+  const padding = maxBaseStr.length;
+  while (curr <= max) {
+    let str = curr.toString(base).padStart(padding, '0');
     combos.push(str);
+    curr++;
   }
-
-  opComboMap[num] = combos;
+  opComboMap[mapKey] = combos;
   return combos;
 }
+
+
+
 export function atLeastOneEquationMakesTestVal(nums, testVal) {
-  const operatorCombos = getOperatorCombos(nums.length - 1);
+  const operatorCombos = getOperatorCombosFromColumnNumber(nums.length - 1, 2);
   let equationsAreTrue = false;
   for (let i = 0; i < operatorCombos.length; i++) {
     let res = nums[0];
