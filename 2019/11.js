6c148994c89ba380d384b98a897a41a36d50c6f6
liamsain <liamkennedy89@outlook.com>
2025-01-09 22:17:57 +0000

    2019 day 11 part 2

--- 2019/11.js
+++ 2019/11.js
@@ -1,61 +1,97 @@
 import { getAdventOfCodeData, logTime } from '../node-utils.js';
 import IntCode from './IntCode.js';
 const input = await getAdventOfCodeData(2019, 11);
+
 const start = performance.now();
+
 const nums = input.split(',').map(x => Number(x));
-
-const currentCoord = [0, 0];
-let facing = 0; // 0 up, 1 right, 2 down, 3 left
-const dict = {};// k = `${x} ${y}`. value will be 0 for black, 1 for white
-
-// if robot is over black panel, give input as 0 else input is 1
-// program will first output value to paint current panel
-// then it will output direction to move in
-// paint current panel, then move in that dir
-
-
-
 let part1 = 0;
-const changeFacing = num => {
-  if (num == 1) {
-    facing = facing == 3 ? 0 : facing + 1;
-  } else {
-    facing = facing == 0 ? 3 : facing - 1;
+
+function runProgram(input = [], dict = {}) {
+  // const dict = {};// k = `${x} ${y}`. value will be 0 for black, 1 for white
+  let lowestx = 0;
+  let highesty = 0;
+  const currentCoord = [0, 0];
+  let facing = 0; // 0 up, 1 right, 2 down, 3 left
+  const changeFacing = (num, currentFacing) => {
+    let result = 0;
+    if (num == 1) {
+      result = currentFacing == 3 ? 0 : currentFacing + 1;
+    } else {
+      result = currentFacing == 0 ? 3 : currentFacing - 1;
+    }
+    return result
   }
-}
-const moveCoords = () => {
-  // + y is up!
-  if (facing == 0) {
-    currentCoord[1] += 1;
-  } else if (facing == 1) {
-    currentCoord[0] += 1;
-  } else if (facing == 2) {
-    currentCoord[1] -= 1;
-  } else {
-    currentCoord[0] -= 1;
+  const moveCoords = (currentFacing, currentCoords) => {
+    const result = [...currentCoords];
+    // + y is up!
+    if (currentFacing == 0) {
+      result[1] += 1;
+    } else if (currentFacing == 1) {
+      result[0] += 1;
+    } else if (currentFacing == 2) {
+      result[1] -= 1;
+    } else {
+      result[0] -= 1;
+    }
+    return result;
   }
+  const initialArg = dict['0 0'] == 1 ? 1 : 0;
+  const ic = new IntCode(nums, [initialArg]);
+  ic.compute();
+  while (!ic.halted) {
+    // paint current tile
+    const paintColour = ic.output[ic.output.length - 2];
+    const key = `${currentCoord[0]} ${currentCoord[1]}`;
+    dict[key] = paintColour;
 
+    // move to next tile
+    let dir = ic.output[ic.output.length - 1];
+    facing = changeFacing(dir, facing);
+    const newCoords = moveCoords(facing, currentCoord);
+    currentCoord[0] = newCoords[0];
+    currentCoord[1] = newCoords[1];
+    if (currentCoord[0] < lowestx) {
+      lowestx = currentCoord[0]
+    }
+    if (currentCoord[1] > highesty) {
+      highesty = currentCoord[1]
+    }
+
+    // get colour of new current tile
+    const newTileKey = `${currentCoord[0]} ${currentCoord[1]}`;
+    const input = dict[newTileKey] ? dict[newTileKey] : 0// default to 0 for black
+
+    ic.pushInputAndContinue([input])
+  }
+  return { dict, lowestx, highesty };
 }
-const ic = new IntCode(nums, [0]);
-ic.compute();
-while (!ic.halted) {
-  // paint current tile
-  let paintColour = ic.output[ic.output.length - 2];
-  const key = `${currentCoord[0]} ${currentCoord[1]}`;
-  dict[key] = paintColour;
+const result = runProgram([0]);
+part1 = Object.keys(result.dict).length;
 
-  // move to next tile
-  let dir = ic.output[ic.output.length - 1];
-  changeFacing(dir);
-  moveCoords();
-
-  // get colour of new current tile
-  const newTileKey = `${currentCoord[0]} ${currentCoord[1]}`;
-  const input = dict[newTileKey] ? dict[newTileKey] : 0// default to 0 for black
-  
-  ic.pushInputAndContinue([input])
+const part2Dict = {};
+part2Dict['0 0'] = 1;
+const result2 = runProgram([1], part2Dict);
+let str = '';
+for (let y = result2.highesty;y > (result2.highesty - 6);y--) {
+  let rowStr = '';
+  for (let x = result2.lowestx; x < (result2.lowestx + 42);x++) {
+    const key = `${x} ${y}`;
+    let ch = '';
+    if (result2.dict[key] == 0) {
+      ch = '.';
+    } else if (result2.dict[key] == 1) {
+      ch = '#';
+    } else {
+      ch = '.';
+    }
+    rowStr += ch;
+  }
+  str += rowStr;
+  str += '\n';
 }
-part1 = Object.keys(dict).length;
+console.log(str);
+
 
 
 const end = performance.now();
--- 2019/9.js
+++ 2019/9.js
@@ -1,9 +1,7 @@
 import { getAdventOfCodeData, logTime } from '../node-utils.js';
 import IntCode from './IntCode.js';
 const input = await getAdventOfCodeData(2019, 9);
-// const input = `109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99`;
-// const input = `1102,34915192,34915192,7,4,7,99,0`
-// const input = `104,1125899906842624,99`;
+
 const start = performance.now();
 const nums = input.split(',').map(x => Number(x))
 const ic = new IntCode(nums, [1]);
